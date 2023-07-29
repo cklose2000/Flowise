5b3698f0129462fb6b729d34816f46a41847667e
cklose2000 <89212817+cklose2000@users.noreply.github.com>
2023-07-29 12:04:43 -0400

    Update logger.ts
    repasted due to formatting issues

--- packages/server/src/utils/logger.ts
+++ packages/server/src/utils/logger.ts
@@ -1,15 +1,15 @@
-import { createLogger, transports, format } from 'winston'
-import { NextFunction, Request, Response } from 'express'
+import { createLogger, transports, format } from 'winston';
+import { NextFunction, Request, Response } from 'express';
 
-const { combine, timestamp, printf, errors } = format
+const { combine, timestamp, printf, errors } = format;
 
 const logger = createLogger({
   format: combine(
     timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.json(),
     printf(({ level, message, timestamp, stack }) => {
-      const text = `${timestamp} [${level.toUpperCase()}]: ${message}`
-      return stack ? text + '\n' + stack : text
+      const text = `${timestamp} [${level.toUpperCase()}]: ${message}`;
+      return stack ? text + '\n' + stack : text;
     }),
     errors({ stack: true })
   ),
@@ -19,14 +19,14 @@ const logger = createLogger({
   transports: [
     new transports.Console()
   ]
-})
+});
 
 export function expressRequestLogger(req: Request, res: Response, next: NextFunction): void {
-  const unwantedLogURLs = ['/api/v1/node-icon/']
+  const unwantedLogURLs = ['/api/v1/node-icon/'];
   if (req.url.includes('/api/v1/') && !unwantedLogURLs.some((url) => req.url.includes(url))) {
-    logger.info(`${req.method} ${req.url}`)
+    logger.info(`${req.method} ${req.url}`);
   }
-  next()
+  next();
 }
 
-export default logger
+export default logger;
